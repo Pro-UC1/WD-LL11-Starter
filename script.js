1329a13796545be4a03328692d2bc35c78f39f23
NEHA RAI <nerai0729@gmail.com>
2025-06-18 18:25:02 +0000

    added groupStationByCity

--- script.js
+++ script.js
@@ -4,19 +4,28 @@ function addStations(stations) {
   for (let i = 0; i < stations.length; i++) {
     addStationElement(stations[i]);
   }
+
+
+  
+
 }
+
+ 
 
 // 🧪 TEAM FEATURES
 
 // 💌 Wishlist Renderer
 function renderWishlist() {
   // TODO: Use forEach to display items in wishlist
+
+
 }
 
 // ❌ Search Feedback
 function searchStations(query) {
   // TODO: Filter stations array based on query
   // TODO: Display error if none found
+
 }
 
 // 🌟 Random Featured Station
@@ -26,13 +35,42 @@ function pickFeaturedStation() {
 
 // 🏙️ Group by City
 function groupStationsByCity() {
-  // TODO: Loop through stations and group under each city
+  
+  // Group stations under each city
+  const grouped = {};
+  stations.forEach(station => {
+    const city = station.city;
+    if (!grouped[city]) {
+      grouped[city] = [];
+    }
+    grouped[city].push(station);
+  });
+  return grouped;
+
 }
+
 
 // 🔄 Filter Toggle
 function toggleFilteredStations() {
   // TODO: Add toggle logic to filter stations dynamically
 }
 
-// Load stations on page start
+// Load stations on page startgr
 addStations(stations);
+
+function renderGroupedStationsByCity() {
+  const grouped = groupStationsByCity();
+  let html = '';
+  for (const city in grouped) {
+    html += `<h3>${city}</h3><ul>`;
+    grouped[city].forEach(station => {
+      html += `<li>${station.name}</li>`;
+    });
+    html += '</ul>';
+  }
+  const container = document.getElementById('city-groups');
+  if (container) {
+    container.innerHTML = html;
+  }
+}
+
